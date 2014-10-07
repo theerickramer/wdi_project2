bf082bed446dfee2c39713fc349dc5d7816ca5c5
theerickramer <theerickramer@gmail.com>
2014-10-07 15:07:39 -0400

    pre heroku

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -170,7 +170,6 @@ $(function(){
 				} 
 			}
 
-			debugger;
 			function createContact(image){
 				if (name.length > 0 && age > 0 && address.length > 0 && phone.length > 0 && picture.length > 0 && cat_id > 0) {
 					debugger;
